bf99cbad57572b84cd9158c8e27d11462804e125
Igindo\FrankPepermans <frank@igindo.com>
2016-12-22 15:38:06 +0100

    fix for out of range

--- lib/src/diff/word_diff.dart
+++ lib/src/diff/word_diff.dart
@@ -10,8 +10,13 @@ class WordDiff extends Diff {
     final List<String> tokens = value.split(new RegExp(r'\b'));
 
     for (int i = 0, len = tokens.length - 1; i < len; i++) {
-      if (tokens[i + 1].isEmpty && tokens[i + 2].isNotEmpty && extendedWordChars.hasMatch(tokens[i]) && extendedWordChars.hasMatch(tokens[i + 2])) {
-        tokens[i] += tokens[i + 2];
+      final String token1up = i + 1 < len ? tokens[i + 1] : null;
+      final String token2up = i + 2 < len ? tokens[i + 2] : null;
+      final bool token1up_empty = token1up == null || token1up.isEmpty;
+      final bool token2up_empty = token2up == null || token2up.isEmpty;
+
+      if (token1up_empty && !token2up_empty && extendedWordChars.hasMatch(tokens[i]) && extendedWordChars.hasMatch(token2up)) {
+        tokens[i] += token2up;
 
         tokens.removeAt(i + 1);
         tokens.removeAt(i + 1);
